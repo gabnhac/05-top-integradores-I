91739023dbf74af1b31b5283e02cf18a78497178
Gabriel Lucena <130411970+gabnhac@users.noreply.github.com>
2025-05-07 01:06:56 +0000

    código backend da pratica com mongoose e dotenv

---
+++ aulaProjetoMongoDb/src/config/mongoose.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/meubanco";
+
+export async function connectDB() {
+    try {
+        await mongoose.connect(MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("MongoDB conectado em ", MONGO_URI);
+    } catch (error) {
+        console.error("Erro ao conectar com mongoDB", error);
+        process.exit(1);
+    }
+}
